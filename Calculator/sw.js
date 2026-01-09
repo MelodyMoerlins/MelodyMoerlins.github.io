d967cb8afaa7f8882632d7e414356f5721dff690
Melody <mmmeeeoldymmmoeee@gmail.com>
2026-01-09 09:29:22 -0600

    Old files deleted

--- Calculator/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', (e) => {
-    console.log('Service worker installed');
-});
-
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        fetch(e.request).catch(() => caches.match(e.request))
-    );
-});
